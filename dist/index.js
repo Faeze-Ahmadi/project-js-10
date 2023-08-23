db89f0daab6e06e94786233d6d10d32044aeefcf
Faeze <faezeahmadi7173@gmail.com>
2023-08-23 23:34:43 +0330

    feat/faeze/js-10/comment-gitignore

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,6 @@
-export {};
+import { ShowContactButton, closeDrawerButton, submitButton } from "./src/importer.js";
+import { showContactButtonHandler, closeContactDrawerHandler, handleCreateContact } from "./src/events.js";
+ShowContactButton?.addEventListener("click", showContactButtonHandler);
+closeDrawerButton?.addEventListener("click", closeContactDrawerHandler);
+submitButton?.addEventListener("click", handleCreateContact);
 //# sourceMappingURL=index.js.map
---
+++ dist/src/importer.js
@@ -0,0 +1,10 @@
+export const contactNameInput = document.querySelector("#contactName");
+export const phoneNumberInput = document.querySelector("#phoneNumber");
+export const deviceStorageInput = document.querySelector("#deviceStorage");
+export const ShowContactButton = document.querySelector("#ShowContactButton");
+export const closeDrawerButton = document.querySelector("#closeDrawerButton");
+export const contactListElement = document.querySelector("#contacts-list");
+export const contactsListDrawer = document.querySelector("#contactsListDrawer");
+export const submitButton = document.querySelector("#submitButton");
+export const contactsList = [];
+//# sourceMappingURL=importer.js.map
