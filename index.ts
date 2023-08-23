d0498490a2f4e97646d586a2087983f712bc3279
Faeze <faezeahmadi7173@gmail.com>
2023-08-23 21:45:32 +0330

    feat/faeze/js-10/impet.ts-to-index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-import { ContactProps , ContacsListType} from "./src/types";
+import { ContactProps , ContacsListType } from "./src/types";
+import { 
+    contactNameInput,
+    phoneNumberInput,
+    deviceStorageInput,
+    ShowContactButton,
+    closeDrawerButton,
+    contactListElement,
+    contactsListDrawer
+} from "./src/importer.js";
--- src/importer.ts
+++ src/importer.ts
@@ -5,8 +5,11 @@ export const phoneNumberInput =
 export const deviceStorageInput =
     document.querySelector<HTMLInputElement>("#deviceStorage");
 export const ShowContactButton =
-    document.querySelector<HTMLButtonElement>("#ShowContactButton");
-
-    
+    document.querySelector<HTMLButtonElement>("#ShowContactButton"); 
 export const closeDrawerButton =
     document.querySelector<HTMLButtonElement>("#closeDrawerButton");
+export const contactListElement =
+    document.querySelector<HTMLUListElement>("#contacts-list");
+export const contactsListDrawer =
+    document.querySelector<HTMLUListElement>("#contactsListDrawer");
+
