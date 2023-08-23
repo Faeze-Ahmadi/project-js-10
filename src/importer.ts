ece5adca1ca6ce3b9e57ff8c9de686ab697ba353
Faeze <faezeahmadi7173@gmail.com>
2023-08-23 20:50:29 +0330

    feat/faeze/js-10/emporter-values

--- src/importer.ts
+++ src/importer.ts
@@ -2,3 +2,11 @@ export const contactNameInput =
     document.querySelector<HTMLInputElement>("#contactName");
 export const phoneNumberInput =
     document.querySelector<HTMLInputElement>("#phoneNumber");
+export const deviceStorageInput =
+    document.querySelector<HTMLInputElement>("#deviceStorage");
+export const ShowContactButton =
+    document.querySelector<HTMLButtonElement>("#ShowContactButton");
+
+    
+export const closeDrawerButton =
+    document.querySelector<HTMLButtonElement>("#closeDrawerButton");
